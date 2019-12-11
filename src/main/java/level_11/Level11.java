f3077098ac682f8accb799872ec8c7b2dbd57b3e
Mikhail Antonov <hermes@cyllene.net>
2019-12-11 09:34:52 +0300

    Level11 part 2

--- src/main/java/level_11/Level11.java
+++ src/main/java/level_11/Level11.java
@@ -9,10 +9,12 @@ import java.util.*;
 
 public class Level11 extends Level {
     IntComp ic;
+    String prog;
     Map<Point, Long> plate = new HashMap<>();
     static List<ImmutablePair<Integer, Integer>> directions = new LinkedList<>();
 
-    static {
+    private static void initDirections() {
+        directions.clear();
         directions.add(ImmutablePair.of(0, -1));
         directions.add(ImmutablePair.of(1, 0));
         directions.add(ImmutablePair.of(0, 1));
@@ -20,36 +22,62 @@ public class Level11 extends Level {
     }
 
     public Level11(String input) {
-        String prog = readResourcesFirstLine(Level11.class, input);
-        ic = new IntComp(prog, 0);
+        initDirections();
+        prog = readResourcesFirstLine(Level11.class, input);
     }
 
     public int p1() {
-        paint();
+        ic = new IntComp(prog, 0);
+        paint(0L);
         return plate.size();
     }
 
-    private void paint() {
+    public String p2() {
+        plate.clear();
+        initDirections();
+        ic = new IntComp(prog, 1);
+        paint(1L);
+        return plateToString();
+    }
+
+    private void paint(long initialPixelState) {
+        plate.put(Point.ZERO, initialPixelState);
         IntComp.ReturnReason lastReason = null;
         Point currentCoordinate = Point.ZERO;
         while (lastReason != IntComp.ReturnReason.HALTED) {
             ic.addToInput(plate.getOrDefault(currentCoordinate, 0L));
             lastReason = ic.run();
-            long color = ic.getOutput().poll();
-            long direction = ic.getOutput().poll();
+            Long color = ic.getOutput().poll();
+            Long direction = ic.getOutput().poll();
             plate.put(currentCoordinate, color);
             // move
-            Collections.rotate(directions, direction == 1L ? 1 : -1);
+            Collections.rotate(directions, Long.valueOf(1L).equals(direction) ? 1 : -1);
             int newX = currentCoordinate.getX() + directions.get(0).getLeft();
             int newY = currentCoordinate.getY() + directions.get(0).getRight();
             currentCoordinate = new Point(newX, newY);
         }
-        System.out.println(plate);
+    }
+
+    private String plateToString() {
+        StringBuilder result = new StringBuilder(System.lineSeparator());
+        int minX = plate.keySet().stream().min(Comparator.comparingInt(Point::getX)).orElseThrow().getX();
+        int maxX = plate.keySet().stream().max(Comparator.comparingInt(Point::getX)).orElseThrow().getX();
+        int minY = plate.keySet().stream().min(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        int maxY = plate.keySet().stream().max(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        for (int i = minY; i <= maxY; i++) {
+            for (int j = maxX; j >= minX; j--) {
+                long pixelColor = plate.getOrDefault(new Point(j, i), 0L);
+                result.append(pixelColor == 0L ? "  " : " #");
+            }
+            result.append(System.lineSeparator());
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
         Level11 l = new Level11("input");
         System.out.println("Part1: " + l.p1());
+        System.out.println("Part2: " + l.p2());
     }
 
 }
