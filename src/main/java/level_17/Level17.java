9d96748faf4f487c383dba211b0d89c6615c1ad1
Mikhail Antonov <hermes@cyllene.net>
2019-12-17 11:19:53 +0300

    cleanup

--- src/main/java/level_17/Level17.java
+++ src/main/java/level_17/Level17.java
@@ -8,6 +8,7 @@ import java.util.Queue;
 public class Level17 extends Level {
     IntComp ic;
     String prog;
+    boolean[][] map;
     private static final boolean VERBOSE = false;
 
     public Level17(String input) {
@@ -18,36 +19,29 @@ public class Level17 extends Level {
     public long p1() {
         ic.run();
         String botOut = readFromBot();
-        return findIntersectionsIn(botOut);
+        map = parseData(botOut.substring(0, botOut.length() - 1));
+        return findIntersectionsIn(map);
     }
 
-    long findIntersectionsIn(String map) {
-        int strLen = map.indexOf("\n");
-        char[] chars = map.toCharArray();
+    long findIntersectionsIn(boolean[][] map) {
         int intersections = 0;
-        int lineNum = 0;
-        for (int i = 1; i < chars.length - (2 * strLen); i++) {
-            if (chars[i] == '\n') {
-                lineNum++;
-                continue;
-            }
-            if (chars[i] == '#'
-                    && chars[i] == '#' // t
-                    && chars[i + 1 + strLen] == '#' // m
-                    && chars[i + 2 + 2 * strLen] == '#' // b
-                    && chars[i + 1 + strLen - 1] == '#' // l
-                    && chars[i + 1 + strLen + 1] == '#' // r
-            ) {
-                // Cross pattern found, intersection in next line below current position
-                int x = (i - lineNum) % strLen;
-                int y = lineNum + 1;
-                if (VERBOSE) System.out.println("Intersection found at: " + x + ":" + y);
-                intersections += x * y;
+        for (int i = 1; i < map.length - 1; i++) {
+            for (int j = 1; j < map[i].length - 1; j++) {
+                if (
+                        map[i][j] // t
+                                && map[i + 1][j] // m
+                                && map[i + 2][j] // b
+                                && map[i + 1][j - 1]  // l
+                                && map[i + 1][j + 1]  // r
+                ) {
+                    // Cross pattern found, intersection in next line below current position
+                    if (VERBOSE) System.out.println("Intersection found at: " + i + ":" + (j - 1));
+                    intersections += i * (j - 1);
+                }
             }
         }
         return intersections;
     }
-
 
     public long p2() {
         long cleaned = 0L;
@@ -94,4 +88,34 @@ public class Level17 extends Level {
         System.out.println("Part1: " + l.p2());
     }
 
+    public boolean[][] parseData(String in) {
+        int w = 0;
+        int h = 0;
+        char[] asChars = in.toCharArray();
+
+        for (int i = 0; i < asChars.length; i++) {
+            if (asChars[i] == '\n') {
+                if (w == 0) {
+                    w = i;
+                }
+                h++;
+            }
+        }
+        boolean[][] result = new boolean[h + 2][w + 2];
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                // All previous newlines + all cells in previous lines + current x
+                int aci = i + j + i * w;
+                if (aci >= asChars.length) {
+                    System.out.printf("w:h %d:%d x:y %d:%d idx %d%n", w, h, j, i, aci);
+                }
+                if (asChars[aci] != '.') {
+                    result[i + 1][j + 1] = true;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/level_17/Level17Test.java
+++ src/test/java/level_17/Level17Test.java
@@ -15,7 +15,12 @@ class Level17Test {
 
     @Test
     void intersectionsShouldBeCountedProperly() {
-        String map = String.join("\n", l.readResources("scaffold0"));
+        // 13 x 7
+        String in = String.join("\n", l.readResources("scaffold0")) + "\n";
+        boolean[][] map = l.parseData(in);
+        assertThat(map).hasSize(9);
+        assertThat(map[0]).hasSize(15).doesNotContain(true);
+        assertThat(map[3][3]).isEqualTo(true); // 2:2 + borders
         assertThat(l.findIntersectionsIn(map)).isEqualTo(76);
     }
 }
