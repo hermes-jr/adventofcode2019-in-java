93cb79b7ffce9b5be385a3da2efc594df7c7de25
Mikhail Antonov <hermes@cyllene.net>
2019-12-18 08:40:56 +0300

    Level 18 data

---
+++ src/test/java/level_18/Level18Test.java
@@ -0,0 +1,17 @@
+package level_18;
+
+import common.Level;
+import org.junit.jupiter.api.Test;
+
+public class Level18Test extends Level {
+    @Test
+    void partOneNumberOfTotalStepsShouldBeCalculatedProperly() {
+        // in0 8
+        // in1 86
+        // in2 132
+        // in3 136
+        // in4 81
+
+    }
+}
+
